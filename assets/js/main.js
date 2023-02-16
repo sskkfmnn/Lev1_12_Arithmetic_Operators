f6d2fd8e5ba8ea6a03c175e35633c8e7e621d4db
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 15:54:26 +0100

    Lev1_12

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+console.log("");
+
+let score = 5+5*10;
+console.log("Ergebnis:" + score);
+
+// let score = (5+5)*10;
+// console.log("Ergebnis:" + score);
+
+// let score = 0;
+// score = score + 10;
+// console.log("Ergebnis:" + score);
+
+// score +=10;
+// console.log("Ergebnis:" + score);
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// console.log(zahl);
+
+// let zahl = 1;
+// zahl ++;
+// console.log("increment:" + zahl);
+
+// zahl +=1;
+// zahl++;
+// console.log("increment:" + Zahl);
+
+// let zahl = 1;
+// zahl --;
+// console.log("decrement:" + zahl);
